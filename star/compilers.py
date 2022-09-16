84f184f65786a5418984616371fca80437bf46b2
Andrew Y. Kwon <andrewkwon256@gmail.com>
2022-09-15 20:18:04 -0400

    Removing dependency on python3.9 removesuffix()

--- star/compilers.py
+++ star/compilers.py
@@ -7,7 +7,8 @@ def compile_to_intermediate(tree):
 # Recursively compile a source node in an abstract syntax tree at a particular depth and indentation level,
 # Returns code which assigns a value to a string _STAR_source_<depth>
 def compile_source_node(node, depth, indent):
-    code = ''
+    # The first line is indented by the caller
+    code = f"# Begin code at depth {depth} with indentation level {indent}\n"
     # Add a line of code, format the line for indentation and end with a new line character
     def append_code(code_line):
         nonlocal code, indent
@@ -15,14 +16,20 @@ def compile_source_node(node, depth, indent):
 
     source_var = '_STAR_source_'
     append_code(f"{source_var}{depth} = ''")
+    add_new_line = False
     for child in node['body']:
+        # Separate each child's output string by a new line
+        if add_new_line:
+            append_code(f"{source_var}{depth} += '\\n'")
+        else:
+            add_new_line = True
+
         if child['id'] == 'TEXT':
-            append_code(f"{source_var}{depth} += {repr(child['body'])} + '\\n'")
+            append_code(f"{source_var}{depth} += {repr(child['body'])}")
         elif child['id'] == 'SUB':
             sub_var = '_STAR_sub_'
             append_code(f"{sub_var}{depth} = ''")
-            # Don't indent already indented code
-            code += compile_source_node(child['body'], depth + 1, indent) + '\n'
+            append_code(compile_source_node(child['body'], depth + 1, indent))
             append_code(f"{sub_var}{depth} = {source_var}{depth+1}")
             for rule in child['rules']:
                 old = rule[0]
@@ -36,16 +43,16 @@ def compile_source_node(node, depth, indent):
             append_code(f"{delim_flag_var}{depth} = False")
             append_code(child['init'])
             append_code(f"while {child['cond']}:")
-            # Don't indent already indented code
-            code += compile_source_node(child['body'], depth + 1, indent + 1) + '\n'
-            append_code(f"    {source_var}{depth+1} = {source_var}{depth+1}.removesuffix('\\n')")
+            append_code(compile_source_node(child['body'], depth + 1, indent + 1))
+            append_code(f"    {source_var}{depth+1} = {source_var}{depth+1}")
             append_code(f"    if {delim_flag_var}{depth}:")
             append_code(f"        {rpt_var}{depth} += {child['delimiter']}")
             append_code(f"    {rpt_var}{depth} += {source_var}{depth+1}")
             append_code(f"    {child['update']}")
             append_code(f"    {delim_flag_var}{depth} = True")
-            append_code(f"{source_var}{depth} += {rpt_var}{depth} + '\\n'")
+            append_code(f"{source_var}{depth} += {rpt_var}{depth}")
         else:
             append_code(f"# {child['id']} abstract syntax tree node not recognized by compiler")
-    # Return code with last newline removed for better control over code formatting
-    return code.removesuffix('\n')
+    # The last line doesn't end with a new line, the caller can add it if necessary
+    code += f"{' '*4*indent}# End code at depth {depth} with indentation level {indent}"
+    return code
